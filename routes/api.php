ed4dbac9b7bed1a65085e25895960d494dffff12
SHAFIQHLFKML21 <mmzguploo@gmail.com>
2022-11-03 15:17:53 +0700

    challange-author

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +25,9 @@ Route::get('me', [AuthController::class, 'me']);
 Route::resource('books', BookController::class)->except(
     ['create','edit']
 );
+
+Route::get('/authors',[AuthorController::class, 'index']);
+Route::post('/authors',[AuthorController::class, 'store']);
+Route::get('/authors/{id}',[AuthorController::class, 'show']);
+Route::put('/authors/{id}',[AuthorController::class, 'update']);
+Route::delete('/authors/{id}',[AuthorController::class, 'destroy']);
